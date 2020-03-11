4ab0a899c9bd0e21975b3cd2dd28422243400e47
lucatraini <lucatra89@gmail.com>
2020-03-11 19:06:54 +0100

    Added experiment number to queryinfo requests

--- ts-travel-service/src/main/java/travel/controller/TravelController.java
+++ ts-travel-service/src/main/java/travel/controller/TravelController.java
@@ -1,5 +1,6 @@
 package travel.controller;
 
+import io.opentracing.Span;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,8 +13,11 @@ import travel.entity.*;
 import travel.service.TravelService;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import static org.springframework.http.ResponseEntity.ok;
+
+import io.opentracing.Tracer;
 
 /**
  * @author fdse
@@ -26,6 +30,18 @@ public class TravelController {
     private TravelService travelService;
 
     private static final Logger LOGGER = LoggerFactory.getLogger(TravelController.class);
+
+    @Autowired
+    private Tracer tracer;
+
+    protected static Random random = new Random(33);
+
+	private void addRequestClass() {
+		int requestClass = random.nextInt(10);
+		Span span = tracer.activeSpan();
+		span.setBaggageItem("experiment", Integer.toString(requestClass));
+		span.setTag("experiment", Integer.toString(requestClass));
+	}
 
     @GetMapping(path = "/welcome")
     public String home(@RequestHeader HttpHeaders headers) {
@@ -99,6 +115,7 @@ public class TravelController {
     @CrossOrigin(origins = "*")
     @PostMapping(value = "/trips/left")
     public HttpEntity queryInfo(@RequestBody TripInfo info, @RequestHeader HttpHeaders headers) {
+        addRequestClass();
         if (info.getStartingPlace() == null || info.getStartingPlace().length() == 0 ||
                 info.getEndPlace() == null || info.getEndPlace().length() == 0 ||
                 info.getDepartureTime() == null) {
