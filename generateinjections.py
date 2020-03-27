a8ab1d6f83f1a72ad5563324df84e9366303b0a7
lucatraini <lucatra89@gmail.com>
2020-03-27 10:51:44 +0100

    Added new generate injection approach

--- generateinjections.py
+++ generateinjections.py
@@ -14,7 +14,14 @@ NUM_REQ_CLASSES = 10
 NUM_NOISED_CLASSES = 5
 MAX_AFFECTED_RPCS_WCLASS = 3
 
-delay = lambda: 50
+## Request std during a 5min load test without injection
+## load test params: (--c) 20 users (--r) 1 hatch rate (--run-time)  5 minutes 
+## user waiting time: wait_time = between(1.0, 3.0) random waiting time between 1 seconds and 3 seconds
+REQ_STD = 5.59
+
+def random_delay(affected_rpcs):
+    delay_ = random.uniform(2*REQ_STD, 3*REQ_STD)/len(affected_rpcs)
+    return int(round(delay_))
 
 
 def read_rpcs(RPCS_PATH):
@@ -43,15 +50,26 @@ def select_affected_asyncrpc(rpcs):
 def add_sync_rpcs(cfg, rpcs, affected_rpcs):
     for rpc in rpcs:
         cfg[rpc] = []
-        for i in range(NUM_REQ_CLASSES):
-            delay_ = delay() if len(affected_rpcs) > i and rpc in affected_rpcs[i] else 0
-            cfg[rpc].append(delay_)
+    
+    for i, affected_rpcs_  in enumerate(affected_rpcs):
+        delay = random_delay(affected_rpcs_)
+        for rpc in rpcs:
+            if rpc in affected_rpcs_:
+                cfg[rpc].append(delay)
+            else:
+                cfg[rpc].append(0)
+
+    for i in range(len(affected_rpcs), NUM_REQ_CLASSES):
+        for rpc in rpcs:
+            cfg[rpc].append(0)
+
     return cfg
 
 def add_async_rpcs(cfg, rpcs, affected_rpc):
+    delay = int(round(random.uniform(2*REQ_STD, 3*REQ_STD)))
     for rpc in rpcs:
         if rpc == affected_rpc:
-            cfg[rpc] = [delay() if i < NUM_NOISED_CLASSES else 0 for i in range(NUM_REQ_CLASSES)]
+            cfg[rpc] = [delay if i < NUM_NOISED_CLASSES else 0 for i in range(NUM_REQ_CLASSES)]
         else:
             cfg[rpc] = [0 for i in range(NUM_REQ_CLASSES)]
     return cfg
